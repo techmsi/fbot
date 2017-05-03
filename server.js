b4f648b3f5004aabf2362ce51c739604f6355800
techmsi <sidromenos@losx-342076-news-n.companynet.org>
2017-05-03 09:59:41 -0400

    Add quick replies

--- server.js
+++ server.js
@@ -1,32 +1,96 @@
 const token = process.env.FB_PAGE_TOKEN;
 const vtoken = process.env.FB_VERIFY_TOKEN;
+const appSecret = process.env.FB_APP_SECRET;
+
 const port = process.env.PORT || 5000;
 
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 const Bot = require('messenger-bot');
 
 let bot = new Bot({
   token: token,
-  verify: vtoken
+  verify: vtoken,
+  app_secret: appSecret
 });
 
 bot.on('error', (err) => {
   console.log(err.message);
 });
 
-bot.on('message', (payload, reply) => {
-  let text = payload.message.text;
+const pickColorMsg = {
+  'text': 'Pick a color:',
+  'quick_replies': [
+    {
+      'content_type': 'text',
+      'title': 'Red',
+      'payload': 'CHOSE_RED'
+    },
+    {
+      'content_type': 'text',
+      'title': 'Green',
+      'payload': 'CHOSE_GREEN'
+    }
+  ]
+};
 
+const handleResponse = (response) => {
+  switch (response) {
+    case 'CHOSE_GREEN':
+      return 'My favorite color is green!';
+      break;
+    case 'CHOSE_RED':
+      return 'I hate red!';
+      break;
+    default:
+      break;
+  }
+};
+
+bot.on('message', (payload, reply) => {
+  let {text, quick_reply} = payload.message;
+  let answer = '';
+  
   bot.getProfile(payload.sender.id, (err, profile) => {
     if (err) throw err;
 
-    reply({ text}, (err) => {
+    reply(pickColorMsg, (err) => {
       if (err) throw err;
 
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`);
-      console.log(JSON.stringify(profile, null, 2));
+      if (quick_reply) {
+        answer = handleResponse(quick_reply.payload);
+      }
+
+      reply({text: answer }, (err) => {});
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${answer} ${text}`);
+      console.log(JSON.stringify(payload, null, 2));
     });
   });
 });
+// Handle verification and Messaging
+let app = express();
 
-http.createServer(bot.middleware()).listen(port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+  return bot._verify(req, res);
+});
+
+app.post('/', (req, res) => {
+  bot._handleMessage(req.body);
+  res.end(JSON.stringify({status: 'ok', reply: req.body || req.body.message || 'Nothing'}));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('Something bad happened', err);
+  }
+
+  console.log(`FB Bot Server is listening on ${port}`);
+});
+
+module.exports = app;
