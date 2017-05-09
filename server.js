157fb1dc96022ba671f956ee90b3e37058f79849
techmsi <sidromenos@losx-342076-news-n.companynet.org>
2017-05-09 09:27:36 -0400

    Add postback method

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const requestify = require('requestify');
 const express = require('express');
 const bodyParser = require('body-parser');
 const Bot = require('messenger-bot');
+const _log = (event, obj) => console.log(`[${event}] Payload -`, JSON.stringify(obj, null, 2));
 
 let bot = new Bot({
   token: token,
@@ -35,13 +36,27 @@ const pickIngredientMsg = {
     }
   ]
 };
-
+const happy = decodeURI('\u2764');
 const _createList = (arr) => arr.map(o => ({
-    title: o.title,
-    image_url: o.thumbnail,
-    subtitle: o.ingredients
-  })
+  title: o.title,
+  image_url: o.thumbnail,
+  subtitle: o.ingredients,
+  buttons: [{
+    type: 'postback',
+    title: `${happy} - ${o.title}`,
+    payload: o.title
+  }]
+})
 );
+
+bot.on('postback', (payload, reply, actions) => {
+  _log('postback', payload);
+  let { postback } = payload;
+
+  reply({ text: `I made "${postback.payload}" last night.
+  ${happy} Mmmm...` }, (err, info) => {
+  });
+});
 
 bot.on('message', (payload, reply) => {
   let {text, quick_reply} = payload.message;
@@ -57,26 +72,27 @@ bot.on('message', (payload, reply) => {
           .then(r => JSON.parse(r.body))
           .then(({results}) => {
             // Get the response body
-            let cards = _createList(results.slice(0,3));
+            let cards = _createList(results.slice(0, 3));
             let recipeCards = {
-                "attachment":{
-                "type":"template",
-                "payload":{
-                   "template_type": "list",
-                    "top_element_style": "compact",
-                    "elements": cards
+              'attachment': {
+                'type': 'template',
+                'payload': {
+                  'template_type': 'list',
+                  'top_element_style': 'compact',
+                  'elements': cards
                 }
               }
             };
 
-            reply(recipeCards, (err) => {});
+            reply(recipeCards, (err) => {
+            });
 
             console.log('recipes', cards.length);
           });
       }
 
       console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`);
-      console.log(JSON.stringify(payload, null, 2));
+      _log('message', payload);
     });
   });
 });
